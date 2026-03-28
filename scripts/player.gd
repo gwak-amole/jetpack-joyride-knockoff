d30bfffd1b402a8e19b14c4914f6ea058541e005
gwak-amole <gwaksophie@gmail.com>
2026-03-27 17:14:45 -0700

    added jetpack effect

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,7 +13,10 @@ func _physics_process(delta):
 	velocity.x = speed_x * direction
 	if Input.is_action_pressed("ui_accept"):
 		velocity.y -= speed_y * delta
-	
+		$CPUParticles2D.emitting = true;
+	if Input.is_action_just_released("ui_accept"):
+		$jetpack_timer.start(0.1)
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	move_and_slide()
@@ -24,3 +27,7 @@ func _physics_process(delta):
 		velocity.y = 0
 		
 	position.x = clamp(position.x, 0+32, screen_size.x-32)
+
+
+func _on_jetpack_timer_timeout() -> void:
+	$CPUParticles2D.emitting = false
